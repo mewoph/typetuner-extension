d1d12722f4884111fce164d5a084b4711479055b
Mew Ophaswongse <mew-mbp@mew-mbp-m1.lan>
2021-05-20 13:06:49 -0700

    Allow axis value to be typed in

--- src/components/FontVariationAxis.vue
+++ src/components/FontVariationAxis.vue
@@ -2,7 +2,13 @@
   <div class="font-variation-axis flex w-full mt-2 space-x-4">
       <div>{{ axisData.tag }}</div>
       <div class="flex w-full space-x-2">
-        <div class="w-12">({{ axisValue }})</div>
+        <div class="w-16">
+          <input type="number"
+            class="w-full shadow"
+            :min="axisData.minValue"
+            :max="axisData.maxValue"
+            :value="axisValue" @change="onChange" />
+        </div>
         <div class="w-12 text-right">{{ axisData.minValue }}</div>
         <input
           type="range"
@@ -35,6 +41,10 @@ export default {
     };
   },
   methods: {
+    onChange(e) {
+      this.axisValue = e.target.value;
+      this.onInput();
+    },
     onInput() {
       const { axisData, axisValue: value } = this;
       this.updateFontVariation({ tag: axisData.tag, value });
